435049548420fe25d6521ee782f77b0a73a2f46e
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-10-22 21:17:05 +0300

    insert node at tail of linked list problem using both approaches(recursion and while loop) SOLVED

---
+++ src/main/java/org/example/InsertNodeAtTail.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class InsertNodeAtTail {
+    public static void main(String[] args) {
+
+        SinglyLinkedListNode head = new SinglyLinkedListNode(5);
+
+        SinglyLinkedListNode chainingNode = head;
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(sc.nextInt());
+            chainingNode.next = node;
+            chainingNode = node;
+        }
+
+        head = insertNodeAtTail(head, 10);
+
+        chainingNode = head;
+        while (chainingNode != null) {
+            System.out.println(chainingNode.data);
+            chainingNode = chainingNode.next;
+        }
+    }
+
+    /**
+     * using recursion
+     */
+//    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+//        if (head == null) {
+//            return new SinglyLinkedListNode(data);
+//        }
+//        if (head.next == null) {
+//            SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+//            head.next = newNode;
+//            return head;
+//        }
+//        insertNodeAtTail(head.next, data);
+//        return head;
+//    }
+
+    /**
+     * using while loop
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        if(head == null){
+            return new SinglyLinkedListNode(data);
+        }
+        SinglyLinkedListNode chainingNode = head;
+        while (chainingNode.next != null) {
+            chainingNode = chainingNode.next;
+        }
+        chainingNode.next = new SinglyLinkedListNode(data);
+
+        return head;
+    }
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+}

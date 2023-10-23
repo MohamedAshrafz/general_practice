8f53771680d519815334b627ce3f130fc38a14cb
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-10-23 18:59:39 +0300

    solution to tree traversal using post/pre/in orders SOLVED

---
+++ src/main/java/org/example/TreeTraversal.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class TreeTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null)
+            return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null)
+            return;
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+}

1e74b8cee0486413c3e1f8e6cd58781d0cc9d87d
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2024-06-29 20:39:26 +0300

    [Regex]Adding phone number validation and optimizer and modifyQueryPagination regexes codes

--- src/main/java/org/example/RegEx.java
+++ src/main/java/org/example/RegEx.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.regex.Pattern;
+
 public class RegEx {
     public static void main(String[] args) {
         String s1 = "abcabcabcabb";
@@ -29,5 +31,24 @@ public class RegEx {
 
         System.out.println(s1.matches("\t"));
 
+    }
+
+    private String getValidPhoneNumber(String str) {
+        String strippedString = str.replace(" ", "");
+
+        if (strippedString.matches("((^\\+9665)|(^9665)|(^009665)|(^5)|(^05))(\\d){8}")) {
+            return strippedString.replaceFirst("((^\\+9665)|(^9665)|(^009665)|(^5)|(^05))", "+9665");
+        } else if (strippedString.matches("((^\\+201)|(^201)|(^00201)|(^01))(\\d){9}")) {
+            return strippedString.replaceFirst("(^\\+201)|(^201)|(^00201)|(^01)", "+201");
+        }
+        return null;
+    }
+
+    private static String modifyQueryPagination(String query, int start, int limit) {
+
+        String returnedQ = query.replaceAll(Pattern.compile("LIMIT\\s*", Pattern.CASE_INSENSITIVE).pattern(), "LIMIT " + limit);
+        returnedQ = returnedQ.replaceAll(Pattern.compile("OFFSET\\s*", Pattern.CASE_INSENSITIVE).pattern(), "OFFSET " + limit);
+
+        return returnedQ;
     }
 }
